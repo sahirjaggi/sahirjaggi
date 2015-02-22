4687eba5886e324e38fa961fe8591bb37e37af80
sahirjaggi <sjaggi95@gmail.com>
2015-02-21 21:52:02 -0500

    Finished Radius page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('.hover').bind('touchstart touchend', function(e) {
+$(document).ready(function () {
+    $('.hover').bind('touchstart touchend', function (e) {
         e.preventDefault();
         $(this).toggleClass('hover_effect');
     });
