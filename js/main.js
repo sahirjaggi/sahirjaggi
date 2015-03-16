5dc62fa68d9ec99356a052a393b614a46dcce02b
sahirjaggi <sjaggi95@gmail.com>
2015-03-16 19:25:10 -0400

    Changed to single page site (not responsive yet tho)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
 $(document).ready(function () {
-    $('.hover').bind('touchstart touchend', function (e) {
-        e.preventDefault();
-        $(this).toggleClass('hover_effect');
+    var slides = $('.main-slide');
+    var i = 0;
+    $('#main-slider-prev').hide();
+    $('#main-slider-next').click(function () {
+        if (i == 0) {
+            $('#main-slider-prev').show();
+        }
+        if (i == slides.length - 2) {
+            $('#main-slider-next').hide();
+        }
+        i = ++i % slides.length;
+        $('.slider-wrapper').animate({
+            'left': -(slides.eq(i).position().left)
+        }, 600, "swing");
     });
+    $('#main-slider-prev').click(function () {
+        if (i == 1) {
+            $('#main-slider-prev').hide();
+        }
+        if (i == slides.length - 1) {
+            $('#main-slider-next').show();
+        }
+        i = --i % slides.length;
+        $('.slider-wrapper').animate({
+            'left': -(slides.eq(i).position().left)
+        }, 600, "swing");
+    });
+    
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
+    
+    $('.fadein img:gt(0)').hide();
+    setInterval(function () {
+        $('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');
+    }, 2000);
 });
